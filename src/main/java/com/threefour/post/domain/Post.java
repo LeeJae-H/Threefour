9408ce20be5a87bf7cb72dc7790b639f0090787b
LeeJaehyung <540900@naver.com>
2025-03-09 21:02:47 +0900

    Feat: Post 엔티티 및 PostTimeInfo 밸류 타입 생성

---
+++ src/main/java/com/threefour/post/domain/Post.java
@@ -0,0 +1,33 @@
+package com.threefour.post.domain;
+
+import com.threefour.user.domain.UserTimeInfo;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "post")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "author_nickname")
+    private String authorNickname;
+
+    @Column(name = "category")
+    private String category;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "content")
+    private String content;
+
+    @Embedded
+    private PostTimeInfo postTimeInfo;
+}
---
+++ src/main/java/com/threefour/post/domain/PostRepository.java
@@ -0,0 +1,6 @@
+package com.threefour.post.domain;
+
+import org.springframework.data.repository.Repository;
+
+public interface PostRepository extends Repository<Post, Long> {
+}
