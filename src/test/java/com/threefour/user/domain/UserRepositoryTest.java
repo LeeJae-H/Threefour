985bec7ea4fa571b68974ffe2c401e3e92b67416
LeeJaehyung <540900@naver.com>
2025-03-11 18:33:09 +0900

    Fix: 테스트 코드(UserRepositoryTest) 리팩토링 - User 인스턴스 생성 로직 메서드로 분리

--- src/test/java/com/threefour/user/domain/UserRepositoryTest.java
+++ src/test/java/com/threefour/user/domain/UserRepositoryTest.java
@@ -1,10 +1,12 @@
 package com.threefour.user.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.util.Optional;
 
@@ -17,13 +19,18 @@ public class UserRepositoryTest {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    void setUp() {
+        jdbcTemplate.update("truncate table user"); // 테스트 전 데이터를 초기화
+    }
+
     @Test
     @DisplayName("사용자 저장")
     void saveUserTest() {
-        String email = "test@naver.com";
-        String password = "testPassword";
-        String nickname = "테스트닉네임";
-        User user = User.join(email, password, nickname);
+        User user = createUserInstance();
 
         // when
         User savedUser = userRepository.save(user);
@@ -31,69 +38,60 @@ public class UserRepositoryTest {
         // then
         assertThat(savedUser).isNotNull();
         assertThat(savedUser.getId()).isNotNull(); // DB에 저장됐는지 여부 확인 -> DB에 저장되지 않으면 Id는 null 값
-        assertThat(savedUser.getEmail()).isEqualTo(email);
-        assertThat(savedUser.getNickname()).isEqualTo(nickname);
+        assertThat(savedUser.getEmail()).isEqualTo(user.getEmail());
+        assertThat(savedUser.getNickname()).isEqualTo(user.getNickname());
     }
 
     @Test
     @DisplayName("Id로 사용자 조회")
     void findUserByIdTest() {
-        String email = "test@naver.com";
-        String password = "testPassword";
-        String nickname = "테스트닉네임";
-        User user = User.join(email, password, nickname);
+        User user = createUserInstance();
 
         // given
         // DB에 사용자가 존재
-        Long userId = userRepository.save(user).getId();
+        User savedUser = userRepository.save(user);
 
         // when
-        Optional<User> foundUser = userRepository.findById(userId);
+        Optional<User> foundUser = userRepository.findById(savedUser.getId());
 
         // then
         assertThat(foundUser.isPresent()).isTrue();
-        assertThat(foundUser.get().getEmail()).isEqualTo(email);
-        assertThat(foundUser.get().getNickname()).isEqualTo(nickname);
+        assertThat(foundUser.get().getEmail()).isEqualTo(savedUser.getEmail());
+        assertThat(foundUser.get().getNickname()).isEqualTo(savedUser.getNickname());
     }
 
     @Test
     @DisplayName("Email로 사용자 조회")
     void findUserByEmailTest() {
-        String email = "test@naver.com";
-        String password = "testPassword";
-        String nickname = "테스트닉네임";
-        User user = User.join(email, password, nickname);
+        User user = createUserInstance();
 
         // given
         // DB에 사용자가 존재
-        userRepository.save(user);
+        User savedUser = userRepository.save(user);
 
         // when
-        Optional<User> foundUser = userRepository.findByEmail(email);
+        Optional<User> foundUser = userRepository.findByEmail(savedUser.getEmail());
 
         // then
         assertThat(foundUser.isPresent()).isTrue();
-        assertThat(foundUser.get().getEmail()).isEqualTo(email);
-        assertThat(foundUser.get().getNickname()).isEqualTo(nickname);
+        assertThat(foundUser.get().getEmail()).isEqualTo(savedUser.getEmail());
+        assertThat(foundUser.get().getNickname()).isEqualTo(savedUser.getNickname());
     }
 
     @Test
     @DisplayName("사용자 삭제")
     void deleteUserTest() {
-        String email = "test@naver.com";
-        String password = "testPassword";
-        String nickname = "테스트닉네임";
-        User user = User.join(email, password, nickname);
+        User user = createUserInstance();
 
         // given
         // DB에 사용자가 존재
-        Long userId = userRepository.save(user).getId();
+        User savedUser = userRepository.save(user);
 
         // when
-        userRepository.delete(user);
+        userRepository.delete(savedUser);
 
         // then
-        Optional<User> deletedUser = userRepository.findById(userId);
+        Optional<User> deletedUser = userRepository.findById(savedUser.getId());
         boolean isExist = deletedUser.isPresent();
         assertThat(isExist).isFalse();
     }
@@ -101,22 +99,26 @@ public class UserRepositoryTest {
     @Test
     @DisplayName("이메일 존재 여부 확인")
     void existsByEmailTest() {
-        String email = "test@naver.com";
-        String password = "testPassword";
-        String nickname = "테스트닉네임";
-        User user = User.join(email, password, nickname);
-        String emailForTestingNotExist = "notUsed@naver.com";
+        User user = createUserInstance();
+        String notExistingEmail = "notUsed@naver.com";
 
         // given
         // DB에 사용자가 존재
-        userRepository.save(user);
+        User savedUser = userRepository.save(user);
 
         // when
-        Boolean isExist = userRepository.existsByEmail(email);
-        Boolean isNotExist = userRepository.existsByEmail(emailForTestingNotExist);
+        Boolean isExist = userRepository.existsByEmail(savedUser.getEmail());
+        Boolean isNotExist = userRepository.existsByEmail(notExistingEmail);
 
         // then
         assertThat(isExist).isTrue();
         assertThat(isNotExist).isFalse();
+    }
+
+    private User createUserInstance() {
+        String email = "test@naver.com";
+        String password = "testPassword";
+        String nickname = "테스트닉네임"; // 처음과 끝에 공백을 넣으면 안됩니다.
+        return User.join(email, password, nickname);
     }
 }
