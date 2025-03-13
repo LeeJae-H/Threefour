f531505ea9eaaaed7d980ee2e2ffb32452089eee
LeeJaehyung <540900@naver.com>
2025-03-13 22:04:26 +0900

    Feat: 회원가입 javascript 수정 - 회원가입 버튼 눌렀을 때 검증된 닉네임인지 확인하는 로직 추가

--- src/main/resources/static/js/join.js
+++ src/main/resources/static/js/join.js
@@ -1,28 +1,34 @@
-let nicknameValidated = false;
+let isNicknameValidated = false;
+let validatedNickname;
 
 // 회원가입
 document.getElementById("joinForm").addEventListener("submit", function (event) {
     event.preventDefault(); // 기본 폼 제출 방지
 
+    const nickname = document.getElementById("nickname").value;
+
     const joinData = {
         email: document.getElementById("email").value,
         password: document.getElementById("password").value,
-        nickname: document.getElementById("nickname").value
+        nickname: nickname
     };
 
-    // 회원가입 요청 보내기
-    axios.post("/api/users/join", joinData)
-        .then(response => {
-            const userNickname = response.data.data;
-            alert(userNickname + "님 환영합니다!");
+    if (nickname != validatedNickname) {
+        alert("닉네임 확인이 필요합니다.");
+    } else {
+        axios.post("/api/users/join", joinData)
+            .then(response => {
+                const userNickname = response.data.data;
+                alert(userNickname + "님 환영합니다!");
 
-            // 리다이렉션
-            const redirectUrl = response.headers['location'];
-            window.location.href = redirectUrl;
-        })
-        .catch(error => {
-            alert("잘못된 비밀번호입니다.");
-        });
+                // 리다이렉션
+                const redirectUrl = response.headers['location'];
+                window.location.href = redirectUrl;
+            })
+            .catch(error => {
+                alert("잘못된 비밀번호입니다.");
+            });
+    }
 });
 
 // 닉네임 검증
@@ -37,7 +43,8 @@ document.getElementById("nicknameButton").addEventListener("click", function (ev
         }})
         .then(response => {
             alert("사용 가능한 닉네임입니다.");
-            nicknameValidated = true;
+            isNicknameValidated = true;
+            validatedNickname = nickname;
             activateJoinButton();
         })
         .catch(error => {
@@ -48,7 +55,7 @@ document.getElementById("nicknameButton").addEventListener("click", function (ev
 // 회원가입 버튼 활성화 여부 확인
 function activateJoinButton() {
     const joinButton = document.getElementById("joinButton");
-    if (nicknameValidated) {
+    if (isNicknameValidated) {
         joinButton.removeAttribute("disabled");
     }
 }
